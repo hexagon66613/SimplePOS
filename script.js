456951132545535f79a239a3c28d25dbf12b1a52
hexagon66613 <reynaldo.oktavianus@seven-retail.com>
2024-09-23 14:54:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,12 @@ document.getElementById('done-transaction').addEventListener('click', () => {
                 amount: totalAmount
             })
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             alert(data.message);
             // Clear selected items and total
